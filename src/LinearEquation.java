54f362d349ed1c4faf29e4cc352dff6f1e0930bf
wendy <wendyz34@nycstudents.net>
2022-10-18 19:03:44 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,17 @@
+public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    public LinearEquation(int x1,int y1,int x2, int y2){
+        if (x1 !=x2 ) {
+            String coord1 = "(x1,y1)";
+            String coord2 = "(x2,y2)";
+        }
+    public double distance(){
+            double dis = (%2f,(y2-y1) / (x2-x1));
+            return dis;
+        }
+
+    }
+}
